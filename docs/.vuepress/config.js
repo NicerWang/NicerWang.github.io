b96359074949ba0c8a28b7e07ddd9812a70ab8e5
Vuegetable <nicer_wang@qq.com>
2022-09-30 18:27:54 +0800

    Update to Vuepress 2.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,35 +1,98 @@
-module.exports = {
-    title: 'NicerWang',
-    description: 'Notes of NicerWang',
-    base: "/",
-    head: [
-      ['link', { rel: 'icon', type: 'image/png', sizes: '196x196', href: '/favicon-196.png' }],
-      ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
-      ['meta', { name: 'theme-color', content: '#fff' }],
-      ['meta', { name: 'baidu-site-verification', content: 'code-4JBlNTh6em' }],
-      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-      ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
-      ['link', { rel: 'mask-icon', href: '/favicon-196.png'}],
-      ['link', { rel: 'apple-touch-icon', href: '/apple-icon-180.png' }]
-    ],
-    themeConfig: {
-      logo: '/icon.jpeg',
-      repo: "NicerWang/NicerWang.github.io",
-      sidebar: 'auto',
-      lastUpdated: "文章采用CC-BY-4.0协议，上次更新时间",
-      nav: [
-        { text: 'Contact', link: '/contact', target: '_blank'},
-      ],
-      smoothScroll: true
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { fullTextSearchPlugin } from "vuepress-plugin-full-text-search2";
+import { pwaPlugin } from '@vuepress/plugin-pwa'
+import { pwaPopupPlugin } from '@vuepress/plugin-pwa-popup'
+import { seoPlugin } from "vuepress-plugin-seo2";
+
+export default defineUserConfig({
+  base: "/",
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Zone of NicerWang',
+      description: 'Zone of NicerWang'
     },
-    plugins: {
-        '@vuepress/plugin-search': true,
-        '@vuepress/back-to-top': true,
-        '@vuepress/pwa': {
-          updatePopup: {
-            message: "New Content is Available.",
-            buttonText: "Refresh"
-          }
-        }
-    },
-  }
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'NicerWang的空间',
+      description: 'NicerWang的空间'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', type: 'image/png', sizes: '196x196', href: '/favicon-196.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }],
+    ['meta', { name: 'theme-color', content: '#03588C' }],
+    ['meta', { name: 'baidu-site-verification', content: 'code-4JBlNTh6em' }],
+    ['meta', { name: 'google-site-verification', content: 'UW47Ln26i4Rlx95yy-5r3G8g_xCkrFTGhVkYo3xKDNQ' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#03588C' }],
+    ['link', { rel: 'mask-icon', href: '/favicon-196.png'}],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-icon-180.png' }]
+  ],
+  plugins: [
+    fullTextSearchPlugin({
+      locales: {
+        '/': {
+          placeholder: 'Full-Text Search',
+        },
+        '/zh/': {
+          placeholder: '全文搜索',
+        },
+      },
+    }),
+    pwaPlugin(),
+    pwaPopupPlugin({
+      locales: {
+        '/': {
+          message: "New content is available",
+          buttonText: "Refresh"
+        },
+        '/zh/': {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        },
+      }
+    }),
+    seoPlugin({
+      hostname: 'NicerWang.github.io',
+      author: 'NicerWang'
+    }),
+  ],
+  theme: defaultTheme({
+    logo: '/icon.jpeg',
+    repo: "NicerWang/NicerWang.github.io",
+    sidebar: 'auto',
+    contributors: false,
+    docsDir: 'docs',
+    smoothScroll: true,
+    locales: {
+      '/': {
+        selectLanguageText: 'Languages',
+        selectLanguageName: 'English',
+        selectLanguageAriaLabel: 'Languages',
+        lastUpdatedText: 'This article is under CC-BY-4.0, Last Modified',
+        toggleColorMode: 'Toggle Color Mode',
+        editLinkText: 'Edit This Page',
+        navbar: [
+          { text: 'HomePage', link: '/' },
+          { text: 'Contact', link: '/Contact.html' }
+        ]
+      },
+      '/zh/': {
+        selectLanguageText: '选择语言',
+        selectLanguageName: '简体中文',
+        selectLanguageAriaLabel: 'Languages',
+        lastUpdatedText: '文章采用CC-BY-4.0协议，上次更新时间',
+        toggleColorMode: '切换夜间模式',
+        editLinkText: '编辑此页',
+        navbar: [
+          { text: '首页', link: '/zh/' },
+          { text: '联系作者', link: '/zh/Contact.html' }
+        ]
+      }
+    }
+  }),
+})
---
+++ docs/zh/README.md
@@ -0,0 +1,29 @@
+---
+title: 主页
+---
+
+# 主页
+
+你好，这里是[NicerWang](https://github.com/NicerWang)的空间！
+
+## 🎯Focus
+
+### 项目
+
+| 名称                                                  | 描述                                   |
+| ----------------------------------------------------- | -------------------------------------- |
+| [DJudger](https://nicerwang.github.io/DJudger)        | 基于 Docker 的代码执行容器             |
+| [nkcsavp](http://nkcsavp.github.io/)                  | 交互式算法可视化平台                   |
+| [AlgoMotion](https://github.com/NicerWang/Algomotion) | 用于算法可视化的轻量级 Canvas 绘制工具 |
+
+### 脚本
+
+| [力扣（Leetcode-cn）清理大师](https://github.com/NicerWang/leetcode-cleaner) |
+| ------------------------------------------------------------ |
+| [BiliBili视频速度控制](https://github.com/NicerWang/Bili_Video_Speed_Controller) |
+
+### [更多...](https://github.com/NicerWang?tab=repositories)
+
+## 📜Blog
+
+* [在Windows上使用AMD显卡进行深度学习](./article/deep_learning_with_amd_gpu_on_windows.md)
